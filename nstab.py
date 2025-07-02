4ff9cc4c04574efe713aa8c62d4dd7cc605d437e
Aleksandr Senkevich <savtog@mail.ru>
2025-07-02 19:23:57 +0300

    Change program file nstab.py v_0.1

--- nstab.py
+++ nstab.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-  
 """  Tables of numbers and ascii symbols (nstub.py).
 
 A console application for Linux that displays the values of numbers
@@ -22,6 +21,7 @@ along with this program. If not, see <https://www.gnu.org/licenses/>."""
 
 
 import sys
+
 
 _SYMBOLS_LIST = [
     'NUL (null)',
@@ -64,28 +64,54 @@ _SYMBOLS_LIST.append('DEL')
 def main(args):
     """Main method"""
 
-    print(args)
-
     print_table_numbers()
     print_table_ascii()
+
 
 def print_table_numbers():
     """Shows numbers from 0 to 15 in binary,
     decimal and hexadecimal formats."""
-    
-    print('\tTable of numbers')
-    print('\t' + '-' * 22)
-    print('Bin\tDec\tHex\t' * 2)
+
+    title = '\n\tTable of number\n\t' + '-' * 16
+    print(title)
+
+    tab_title = 'Bin\tDec\tHex\t' * 2
+    print(tab_title)
+
+    tab_str_fmt = '{0:04b}\t{0:>3d}\t{0:>3x}\t{1:04b}\t{1:>3d}\t{1:>3x}'
     for i in range(8):
-        print('{0:04b}\t{0:>3d}\t{0:>3x}\t{1:04b}\t{1:>3d}\t{1:>3x}'.format(i, i + 8))
+        tab_str = tab_str_fmt.format(i, i + 8)
+        print(tab_str)
+
 
 def print_table_ascii():
     """Shows table of ASCII symbols."""
-    print('\n\tTable ASCII')
-    print('\t' + '-' * 11)
-    print('Bin       Dec  Hex  Symbol                       Bin       Dec  Hex  Symbol  Bin       Dec  Hex  Symbol  Bin       Dec  Hex  Symbol')
+
+    title = '\n\tTable ASCII\n\t' + '-' * 11
+    print(title)
+
+    tab_title_fmt = '{0:8s}  {1:3s}  {2:3s}  {3:28s}  ' \
+        '{0:8s}  {1:3s}  {2:3s}  {3:6s}  ' \
+        '{0:8s}  {1:3s}  {2:3s}  {3:6s}  ' \
+        '{0:8s}  {1:3s}  {2:3s}  {3:6s}'
+    tab_title = tab_title_fmt.format('Bin', 'Dec', 'Hex', 'Symbol')
+    print(tab_title)
+
+    table_str_fmt = '{0:08b}  {0:>3d}  {0:>3x}  {1:28s}  ' \
+        '{2:08b}  {2:>3d}  {2:>3x}  {3:6s}  ' \
+        '{4:08b}  {4:>3d}  {4:>3x}  {5:6s}  ' \
+        '{6:08b}  {6:>3d}  {6:>3x}  {7:6s}'
     for i in range(len(_SYMBOLS_LIST)//4):
-        print('{0:08b}  {0:3d}  {0:3x}  {1:28s} {2:08b}  {2:3d}  {2:3x}  {3:6s}  {4:08b}  {4:3d}  {4:3x}  {5:6s}  {6:08b}  {6:3d}  {6:3x}  {7:6s}'.format(i, _SYMBOLS_LIST[i], i + 32, _SYMBOLS_LIST[i + 32], i + 32 * 2, _SYMBOLS_LIST[i + 32 * 2], i + 32 * 3, _SYMBOLS_LIST[i + 32 * 3]))
+        symbol0 = _SYMBOLS_LIST[i]
+        symbol1 = _SYMBOLS_LIST[i + 32]
+        symbol2 = _SYMBOLS_LIST[i + 32 * 2]
+        symbol3 = _SYMBOLS_LIST[i + 32 * 3]
+        table_str = table_str_fmt.format(
+            i, symbol0,
+            i + 32, symbol1,
+            i + 32 * 2, symbol2,
+            i + 32 * 3, symbol3)
+        print(table_str)
 
 
 if __name__ == '__main__':
